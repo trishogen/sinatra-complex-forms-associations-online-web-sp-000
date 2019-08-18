0823b6cc0c808f33d214dd06619b402517adb574
Trish Ogen <patriciaogen@gmail.com>
2019-08-18 03:31:46 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -46,7 +46,6 @@ class PetsController < ApplicationController
       @pet.owner << Owner.create(name: params["owner"]["name"])
     end
 
-    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
