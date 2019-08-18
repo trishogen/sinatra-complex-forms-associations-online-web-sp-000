7ebaee656ee31b3b19f722dbe751582eccec0df6
Trish Ogen <patriciaogen@gmail.com>
2019-08-18 03:14:23 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,14 +12,14 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
-    
+
     if !params["owner"]["name"].empty?
-      @pet.owner << Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
 
     @pet.save
 
-    redirect to "/pets/#{@pet.id}"
+    redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id/edit' do
