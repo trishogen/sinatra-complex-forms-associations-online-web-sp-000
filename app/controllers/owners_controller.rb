95da8449ea67c0ce39bd70251fec8d87b3d0c2ac
Trish Ogen <patriciaogen@gmail.com>
2019-08-18 02:23:31 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -20,7 +20,7 @@ class OwnersController < ApplicationController
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
-    # erb :'/owners/edit'
+    erb :'/owners/edit'
   end
 
   get '/owners/:id' do
